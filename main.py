0fdee8f259b3b977fd26e61717295b3a255da390
Kalkin <50224596+kalkulator413@users.noreply.github.com>
2022-03-15 11:57:15 -0700

    added new line for p^2 + 2pq + q^2 to demonstrate that it's always = to 1

--- main.py
+++ main.py
@@ -9,22 +9,27 @@ num_generations = int(input())
 
 counter = 1
 x_vals = [0]
-y_vals = [p]
+p_vals = [p]
+q_vals = [1-p]
 for _ in range(num_generations):
     print('p =', p, 'q = ', 1-p)
-    p = run(p, num_offspring=20000)
+    p = run(p, num_offspring=2000)
     x_vals.append(counter)
     counter += 1
-    y_vals.append(p)
+    p_vals.append(p)
+    q_vals.append(1-p)
 
 fig, ax = plt.subplots()
 
-ax.plot(x_vals, y_vals, linewidth=2.0)
-ax.plot(x_vals, [1- y for y in y_vals], linewidth=2.0)
+ax.plot(x_vals, p_vals, linewidth=2.0)
+ax.plot(x_vals, q_vals, linewidth=2.0)
+ax.plot(x_vals, [p_vals[i]**2 + 2*p_vals[i]*q_vals[i] + q_vals[i]**2 for i in range(num_generations + 1)], 
+    linewidth=6.0)
 
 plt.xlabel("Number of Generations")
 plt.ylabel("Allele Frequency")
-plt.legend(["p", "q"])
+plt.legend(["p", "q", 'p^2 + 2pq + q^2'])
 plt.ylim([0, 1])
+plt.xlim([0, num_generations + 1])
 
 plt.show()
